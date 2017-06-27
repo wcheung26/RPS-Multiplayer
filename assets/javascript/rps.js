838022ef6cc7c061caf3a87d5fc38efb41fc3404
wcheung26 <william.cheung94@gmail.com>
2017-06-27 16:38:29 -0700

    add initial files

---
+++ assets/javascript/rps.js
@@ -0,0 +1,30 @@
+// Player one enters name
+
+// Push child with name and score 0
+
+// Pull player 1 name and score with on("value")
+
+// Player two enters name 
+
+// Push child with name and score 0
+
+// Pull player 2 name and score with on("value")
+
+// Player 1 show options 
+
+// On click push choice to player 1
+
+// Player 2 show options 
+
+// On click push choice to player 2
+
+// Pull choices with on("value") 
+
+
+// Calculate who wins and display result in middle
+
+// Push wins/losses 
+
+
+
+
